191d24cba4290a958195076e44f4b29f40cc8b8d
Cyril Rol <crol@efaktory.fr>
2020-09-25 11:55:23 +0200

    Open Popup with geoip country

--- Taxes/view/frontend/web/js/modal.js
+++ Taxes/view/frontend/web/js/modal.js
@@ -4,8 +4,15 @@ require([
     'Magento_Customer/js/customer-data'
 ], function ($, modal,customerData ) {
 
+  'use strict';
+
+    // reload of geoip_country section
+    var sections = ['geoip_country'];
+    customerData.invalidate(sections);
+    customerData.reload(sections, true);
+
     let getcookie = $('#getCookie').val()
-    let cookie = getcookie ? false : true
+    let cookieExist = getcookie ? false : true
 
     var options = {
         type: 'popup',
@@ -55,30 +62,37 @@ require([
 
     $(document).ready(function () {
         var popup = modal(options, $('#transiteo-modal'))
-        //$("#transiteo-modal").modal("openModal");
+
+        //get visitor country based on geoip
+        customerData.get('geoip_country').subscribe(function(value) {
+
+          if(!value.same_country_as_website && !cookieExist){
+            $("#transiteo-modal").modal("openModal");
+            
+          }
+        });
 
         $(document).on('click', '#click-me', function () {
             var popup = modal(options, $('#transiteo-modal'))
             $('#transiteo-modal').modal('openModal')
         })
 
-        jQuery.ajax({
-            url: '/transiteo/test/view',
+        /*jQuery.ajax({
+            url: 'transiteo/test/view',
             type: 'GET',
             success: function (data) {
 
-                if (!data['same_country']) {
+                if (!data['same_country'] && !cookieExist) {
                     var popup = modal(options, $('#transiteo-modal'))
                     $('#transiteo-modal').modal('openModal')
                 }
 
             },
             error: function (result) {
-                console.log('no response !')
+                console.log('Error catching visitor ip adress in ajax')
             }
-        })
+        })*/
 
-        console.log(customerData.get('geoip_country'));
-    })
+      })
 
 })
