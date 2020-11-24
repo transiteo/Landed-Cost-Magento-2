8b18643212869a2f0d4bb433d653849fd091cc0b
Bruno FACHE <bruno@bird.eu>
2020-11-24 11:38:44 +0100

    Fixing error in choose country pop up :  - fix bad cookie get  - fix error on magento 2.4.1

--- Taxes/view/frontend/web/js/geoip_country.js
+++ Taxes/view/frontend/web/js/geoip_country.js
@@ -6,6 +6,11 @@ define([
 
     // reload of geoip_country section
     var sections = ['geoip_country'];
+    try{
+        customerData.initStorage();
+    }catch (e){
+        console.log("Version of magento is not 2.4.1");
+    }
     customerData.invalidate(sections);
     customerData.reload(sections, true);
 
