f941ac9ac5284371468e9d95b16898f67a806e18
Bruno FACHE <bruno@bird.eu>
2021-10-07 14:04:19 +0200

    Update rendering of Taxes in Invoices, Credit Memo ...

--- LandedCost/Model/Sales/Pdf/Taxes.php
+++ LandedCost/Model/Sales/Pdf/Taxes.php
@@ -30,104 +30,39 @@ class Taxes extends \Magento\Sales\Model\Order\Pdf\Total\DefaultTotal
     public function getTotalsForDisplay()
     {
         /**
-         * @var TransiteoTaxesExtensionInterface $order
+         * @var TransiteoTaxesExtensionInterface $salesEntity
          */
-        $order = $this->getOrder();
+        $salesEntity = $this->getSource();
 
+        $isCreditMemo = false;
+        $isInvoice = false;
+        if($salesEntity->getEntityType() === "creditmemo"){
+            $isCreditMemo = true;
+        }elseif ($salesEntity->getEntityType() === "invoice"){
+            $isInvoice = true;
+        }
 
-        $totals = [];
-        if ($order) {
-            $transiteoTotalTaxes = $order->getTransiteoTotalTaxes();
-            if($transiteoTotalTaxes){
-                $baseTransiteoTotalTaxes = $order->getBaseTransiteoTotalTaxes();
-                $transiteoDuty = $order->getTransiteoDuty();
-                $baseTransiteoDuty = $order->getBaseTransiteoDuty();
-                $transiteoVat = $order->getTransiteoVat();
-                $baseTransiteoVat = $order->getBaseTransiteoVat();
-                $transiteoSpecialTaxes = $order->getTransiteoSpecialTaxes();
-                $baseTransiteoSpecialTaxes = $order->getBaseTransiteoSpecialTaxes();
-                $included = $order->getTransiteoIncoterm();
-                if ($included === "ddp") {
-                    $included = ' ' . __('(included)');
-                } else {
-                    $included = ' ' . __('(not included)');
+        $incoterm = $salesEntity->getTransiteoIncoterm();
+        if (isset($incoterm) && (!$isCreditMemo || $incoterm==="ddp")) {
+            if ($incoterm === "ddp") {
+                $included = ' (' . __('included') . ')';
+                if($isCreditMemo || $isInvoice){
+                    $included = '';
                 }
-
-                $totalSetted = 0;
-                if (isset($transiteoVat)) {
-                    $totalSetted++;
-                }
-                if (isset($transiteoDuty)) {
-                    $totalSetted++;
-                }
-                if (isset($transiteoSpecialTaxes)) {
-                    $totalSetted++;
-                }
-
-                if (($totalSetted > 1)) {
-                    $totals['transiteo_total_taxes'] = new \Magento\Framework\DataObject(
-                        [
-                            'code' => 'transiteo_total_taxes',
-                            'field' => 'transiteo_total_taxes_amount',
-                            'strong' => true,
-                            'value' => $transiteoTotalTaxes,
-                            'base_value' => $baseTransiteoTotalTaxes,
-                            'label' => __('Duty & Taxes Total' . $included),
-                        ]
-                    );
-                }
-
-                if (isset($transiteoSpecialTaxes)) {
-                    $totals['transiteo_special_taxes'] = new \Magento\Framework\DataObject(
-                        [
-                            'code' => 'transiteo_special_taxes',
-                            'field' => 'transiteo_special_taxes_amount',
-                            'value' => $transiteoSpecialTaxes,
-                            'base_value' => $baseTransiteoSpecialTaxes,
-                            'label' => __('Special Taxes SubTotal' . $included),
-                        ]
-                    );
-                }
-
-                if (isset($transiteoVat)) {
-                    $totals['transiteo_vat'] = new \Magento\Framework\DataObject(
-                        [
-                            'code' => 'transiteo_vat',
-                            'field' => 'transiteo_vat_amount',
-                            'value' => $transiteoVat,
-                            'base_value' => $baseTransiteoVat,
-                            'label' => __('VAT/GST SubTotal' . $included),
-                        ]
-                    );
-                }
-
-                if (isset($transiteoDuty)) {
-                    $totals['transiteo_duty'] = new \Magento\Framework\DataObject(
-                        [
-                            'code' => 'transiteo_duty',
-                            'field' => 'transiteo_duty_amount',
-                            'value' => $transiteoDuty,
-                            'base_value' => $baseTransiteoDuty,
-                            'label' => __('Duty SubTotal' . $included),
-                        ]
-                    );
-                }
+            } else {
+                $included = ' (' . __('not included') . ')';
             }
         }
-        return $totals;
-    }
 
-    /**
-     * Check if we can display Weee total information in PDF
-     *
-     * @return bool
-     */
-    public function canDisplay()
-    {
-        /**
-         * @var TransiteoTaxesExtensionInterface $orderAttributes
-         */
-        $amount = $this->getOrder()->getTransiteoTotalTaxes();
-        return $this->getDisplayZero() === 'true' || $amount != 0;
+        $amount = $this->getOrder()->formatPriceTxt($this->getAmount());
+        if ($this->getAmountPrefix()) {
+            $amount = $this->getAmountPrefix() . $amount;
+        }
+
+        $label = __($this->getTitle()) . $included;
+
+        $fontSize = $this->getFontSize() ? $this->getFontSize() : 7;
+        $total = ['amount' => $amount, 'label' => $label, 'font_size' => $fontSize];
+        return [$total];
     }
 }
